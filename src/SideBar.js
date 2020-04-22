260bb4d273693e4c1aa0557cee01e8bb9653d343
Wengel G <wengelg@Wengels-MBP.fios-router.home>
2020-04-22 18:14:48 -0400

    sidebar routing

--- src/SideBar.js
+++ src/SideBar.js
@@ -11,25 +11,50 @@ import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemText from '@material-ui/core/ListItemText'
 
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 // Wengel
 export default function SideBar() {
     return (
     <div className="Side">
         <List disablePadding dense>
-            <div className="s">
-                <ListItem button>
-                    <ListItemText primary="Signup/Login" secondary= {Login} ></ListItemText>
-                </ListItem>
-            </div>
-            <ListItem button>
-                <ListItemText primary="Profile" secondary= {Profile}></ListItemText>
-            </ListItem>
-            <ListItem button>
-                <ListItemText primary="Home" secondary= {Home}></ListItemText>
-            </ListItem>
-            <ListItem button>
-                <ListItemText primary="Chatroom" secondary= {ChatRoom}></ListItemText>
-            </ListItem>
+            <Router>
+                <div className="s">
+                    <Switch>
+                        <ListItem button>
+                            <ListItemText primary="Signup/Login" secondary= {Login} ></ListItemText>
+                        </ListItem>
+                        <Route exact path="/Login" component={Login}></Route>
+                        </Switch>
+                </div>
+            </Router>
+
+            <Router>
+                <Switch>
+                    <ListItem button>
+                        <ListItemText primary="Profile" secondary= {Profile}></ListItemText>
+                    </ListItem>
+                    <Route exact path="/Profile" component={Profile}></Route>
+                </Switch>
+            </Router>
+            
+            <Router>
+                <Switch>
+                    <ListItem button>
+                        <ListItemText primary="Home" secondary= {Home}></ListItemText>
+                    </ListItem>
+                    <Route exact path="/Home" component={Home}></Route>
+                </Switch>
+            </Router>
+
+            <Router>
+                <Switch>
+                    <ListItem button>
+                        <ListItemText primary="ChatRoom" secondary= {ChatRoom}></ListItemText>
+                    </ListItem>
+                    <Route exact path="/ChatRoom" component={ChatRoom}></Route>
+                </Switch>
+            </Router>
         </List>
     </div>
     );
